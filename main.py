87dedfff1a2c779940b7b710c38e99200350cd21
Donjella <brendonang@gmail.com>
2024-11-16 11:06:28 +1100

    Implement Read operation on student entity

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
+
 from flask import Flask
+
 from init import db, ma
 from controllers.cli_controller import db_commands
+from controllers.student_controller import students_bp
+
 
 def create_app():
     app = Flask(__name__)
-
-    print("Server started")
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 
@@ -14,5 +16,6 @@ def create_app():
     ma.init_app(app)
 
     app.register_blueprint(db_commands)
-    
+    app.register_blueprint(students_bp)
+
     return app
