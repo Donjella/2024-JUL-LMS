65502a0a80feb9595f727b759c14a66d5414093e
Donjella <brendonang@gmail.com>
2024-11-30 10:05:47 +1100

    Add global error handlers for 400 and 404

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ def create_app():
     @app.errorhandler(ValidationError)
     def validation_error(err):
         return {"message": err.messages}, 400
+    
+    # in case we forgot to handle errors such as 400, 404, the global handlers below will cover those
+    @app.errorhandler(400)
+    def bad_request(err):
+        return {"message": str(err)}, 400
+    
+    @app.errorhandler(404)
+    def bad_request(err):
+        return {"message": str(err)}, 400
 
     app.register_blueprint(db_commands)
     app.register_blueprint(students_bp)
